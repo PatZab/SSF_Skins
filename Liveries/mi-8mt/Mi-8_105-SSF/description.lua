a6f5a5fb89ad39b61dbe1a6193df73e35e6bc8e7
funkpunkt <funkpunkt@gmx.de>
2025-01-13 00:25:57 +0100

    updated Ka-50_3 liveries to v1.1 and added Mi-8MT liveries v1.0

---
+++ Liveries/mi-8mt/Mi-8_105-SSF/description.lua
@@ -0,0 +1,146 @@
+livery = {
+    {"mi_8_tex1", 0 ,"mi_8_tex_1_105SSF",false};
+	{"mi_8_tex1", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};
+	
+	{"mi_8_tex2", 0 ,"mi_8_tex_2_105SSF",false};
+	{"mi_8_tex2", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};
+	
+	{"mi_8_tex3", 0 ,"mi_8_tex_3_105SSF",false};
+	{"mi_8_tex3", ROUGHNESS_METALLIC ,"mi_8_tex_3_Mat_RoughMet",true};
+	
+	{"mi_8_tex4", 0 ,"mi_8_tex_4_105SSF",false};
+	{"mi_8_tex4", ROUGHNESS_METALLIC ,"mi_8_tex_4_Mat_RoughMet",true};
+	
+	
+	{"EVU", 0 , "evu_texture_105SSF", false};
+
+	
+	
+	
+	---- БОРТОВЫЕ НОМЕРА
+	
+	{"mi_8_ENGINE_NUMBER_001", 0 ,"mi_8_tex_1_105SSF",false};             -- трехзначный бортовой номер. расположение - в районе воздухозаборников двигателей Three-digit tail number. Location - in the area of ​​the engine air intakes
+	{"mi_8_ENGINE_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};        --       
+	{"mi_8_ENGINE_NUMBER_001", DECAL ,"empty",true};              --
+
+    {"mi_8_ENGINE_NUMBER_010", 0 ,"mi_8_tex_1_105SSF",false};             --    
+	{"mi_8_ENGINE_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};        --       
+	{"mi_8_ENGINE_NUMBER_010", DECAL ,"empty",true};              --
+
+    {"mi_8_ENGINE_NUMBER_100", 0 ,"mi_8_tex_1_105SSF",false};             --    
+	{"mi_8_ENGINE_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};        --       
+	{"mi_8_ENGINE_NUMBER_100", DECAL ,"empty",true};              --
+ 
+    
+	
+	{"mi_8_SMALL_NUMBER_001", 0 ,"mi_8_tex_1_105SSF",false};             -- трехзначный бортовой номер. расположение - на борту под воздухозаборниками двигателей Three-digit tail number. Location - On board under engine air intakes
+	{"mi_8_SMALL_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};        --       
+	{"mi_8_SMALL_NUMBER_001", DECAL ,"empty",true};              --
+
+    {"mi_8_SMALL_NUMBER_010", 0 ,"mi_8_tex_1_105SSF",false};             --    
+	{"mi_8_SMALL_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};        --       
+	{"mi_8_SMALL_NUMBER_010", DECAL ,"empty",true};              --
+
+    {"mi_8_SMALL_NUMBER_100", 0 ,"mi_8_tex_1_105SSF",false};             --    
+	{"mi_8_SMALL_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};        --       
+	{"mi_8_SMALL_NUMBER_100", DECAL ,"empty",true};              --
+	
+	
+	
+	
+	
+	{"mi_8_BORT_NUMBER_001", 0 ,"mi_8_tex_1_105SSF",false};               -- трехзначный бортовой номер. расположение - в центре корпуса на борту Three-digit tail number. Location - in the center of the hull on the side
+	{"mi_8_BORT_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};          --     
+	{"mi_8_BORT_NUMBER_001", DECAL ,"empty",true};     --
+
+    {"mi_8_BORT_NUMBER_010", 0 ,"mi_8_tex_1_105SSF",false};               --  
+	{"mi_8_BORT_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};          --     
+	{"mi_8_BORT_NUMBER_010", DECAL ,"empty",true};     --
+
+    {"mi_8_BORT_NUMBER_100", 0 ,"mi_8_tex_1_105SSF",false};               --  
+	{"mi_8_BORT_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_1_Mat_RoughMet",true};          --     
+	{"mi_8_BORT_NUMBER_100", DECAL ,"empty",true};     --       
+	
+	{"mi_8_BALKA_2_1_NUMBER_001", 0 ,"mi_8_tex_2_105SSF",false};          -- трехзначный бортовой номер. расположение - у корня хвостовой балки Three-digit tail number. Location - at the root of the tail boom
+	{"mi_8_BALKA_2_1_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_2_1_NUMBER_001", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_2_1_NUMBER_010", 0 ,"mi_8_tex_2_105SSF",false};          --       
+	{"mi_8_BALKA_2_1_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_2_1_NUMBER_010", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_2_1_NUMBER_100", 0 ,"mi_8_tex_2_105SSF",false};          --       
+	{"mi_8_BALKA_2_1_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_2_1_NUMBER_100", DECAL ,"empty",true};           --
+
+
+	
+	{"mi_8_BALKA_1_1_NUMBER_001", 0 ,"mi_8_tex_2_105SSF",false};          -- трехзначный бортовой номер. расположение - на хвостовой балке с левой стороны Three-digit tail number. Location - on the tail boom on the left side
+	{"mi_8_BALKA_1_1_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_1_NUMBER_001", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_1_1_NUMBER_010", 0 ,"mi_8_tex_2_105SSF",false};          --       
+	{"mi_8_BALKA_1_1_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_1_NUMBER_010", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_1_1_NUMBER_100", 0 ,"mi_8_tex_2_105SSF",false};          --       
+	{"mi_8_BALKA_1_1_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_1_NUMBER_100", DECAL ,"empty",true};           --
+
+	
+	
+	{"mi_8_BALKA_1_2_NUMBER_001", 0 ,"mi_8_tex_2_105SSF",false};          -- трехзначный бортовой номер. расположение - на хвостовой балке с правой стороны Three-digit tail number. Location - on the tail boom on the right side        
+	{"mi_8_BALKA_1_2_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_2_NUMBER_001", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_1_2_NUMBER_010", 0 ,"mi_8_tex_2_105SSF",false};          --      
+	{"mi_8_BALKA_1_2_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_2_NUMBER_010", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_1_2_NUMBER_100", 0 ,"mi_8_tex_2_105SSF",false};          --      
+	{"mi_8_BALKA_1_2_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_2_NUMBER_100", DECAL ,"empty",true};           --
+	
+	
+	
+	{"mi_8_BALKA_1_3_NUMBER_001", 0 ,"mi_8_tex_2_105SSF",false};          -- дополнительный трехзначный бортовой номер. расположение - по середине хвостовой балке с правой стороны Additional three-digit tail number. Location - in the middle of the tail boom on the right side
+	{"mi_8_BALKA_1_3_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_3_NUMBER_001", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_1_3_NUMBER_010", 0 ,"mi_8_tex_2_105SSF",false};          --       
+	{"mi_8_BALKA_1_3_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_3_NUMBER_010", DECAL ,"empty",true};           --
+
+    {"mi_8_BALKA_1_3_NUMBER_100", 0 ,"mi_8_tex_2_105SSF",false};          --       
+	{"mi_8_BALKA_1_3_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_BALKA_1_3_NUMBER_100", DECAL ,"empty",true};           -- 
+
+	
+	{"mi_8_SMALL_BALKA_NUMBER_001", 0 ,"mi_8_tex_2_105SSF",false};          --  место под трехзначный регистрационный номер. расположение - на хвостовой балке перед горизонтальным оперением Space for three-digit registration number. Location - on the tail boom in front of the horizontal tail       
+	{"mi_8_SMALL_BALKA_NUMBER_001", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_SMALL_BALKA_NUMBER_001", DECAL ,"empty",true};           --
+
+    {"mi_8_SMALL_BALKA_NUMBER_010", 0 ,"mi_8_tex_2_105SSF",false};          --      
+	{"mi_8_SMALL_BALKA_NUMBER_010", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_SMALL_BALKA_NUMBER_010", DECAL ,"empty",true};           --
+
+    {"mi_8_SMALL_BALKA_NUMBER_100", 0 ,"mi_8_tex_2_105SSF",false};          --      
+	{"mi_8_SMALL_BALKA_NUMBER_100", ROUGHNESS_METALLIC ,"mi_8_tex_2_Mat_RoughMet",true};     --          
+	{"mi_8_SMALL_BALKA_NUMBER_100", DECAL ,"empty",true};           --
+	
+	
+	
+	{"pilot_MI8_patch", 0, "empty", true}
+	
+	
+}
+name = "105th Shadow Strike Force"
+name_ru = "105-я теневая ударная сила"
+--countries = {"GER"}
+order = 1
+
+-- custom_args = 
+-- {
+	-- [457] = 1.0, -- EVU off
+-- }
+
