eba52f73dc326028695560c669d1d509f31d98f3
funkpunkt <funkpunkt@gmx.de>
2025-01-14 20:00:29 +0100

    added Su-25T v1.0 and Mi-24P v1.0 liveries

---
+++ Liveries/su-25t/Su-25T_105-SSF/description.lua
@@ -0,0 +1,53 @@
+livery = {
+	-- Rumpfmittelteil
+    {"fuz-center", 0, "SU39-PAINT1-DEF-01_SSF", false},
+
+    -- Tragflächen
+	{"wings", 0, "SU39-PAINT2-DEF-01_SSF", false},
+
+    -- unauffindbar
+	{"SU39-PAINT2bis-DEF-01", 0, "SU39-PAINT2bis-DEF-01_SSF", false},
+
+    -- Heck und Seitenleitwerk
+	{"fuz-tail & fin", 0, "SU39-PAINT3-DEF-01_SSF", false},
+
+    -- Höhenleitwerk
+	{"tail & stab", 0, "SU39-PAINT4-DEF-01_SSF", false},
+    
+	-- Seitengondeln
+	{"brake-L", 0, "SU39-PAINT5-L-DEF-01_SSF", false},
+	{"brake-R", 0, "SU39-PAINT5-R-DEF-01_SSF", false},
+    
+	-- Nase und Cockpit
+	{"fuz-nose-steel", 0, "SU39-PAINT6-DEF-01-4K_SSF", false},
+    
+	-- Innenpylonen
+	{"SU-39-PILON", 0, "SU39-PILON-PAINT-DEF-01_SSF", false},
+    
+	-- Außenpylone
+	{"PILON_2_Su-39", 0, "SU39-PILON-2-PAINT-DEF-01_SSF", false},
+	
+	--{"numbers", 0, "rus-code-numbers-ver1-bww-x_gre", false},
+    --{"numbers", 0, "rus-code-numbers-ver1-B-w-W", true},
+    --empty{"numbers", 0, "rus-code-numbers-ver1-bww-x_gre", false},
+    {"numbers", 0, "mi-24p_ext06_bn_syzran", false},
+    
+	-- unbekannt, evtl. Außenpylone
+	{"PylonsSu-25T1", 0, "Su-25TPylon_SSF", false},
+
+	-- Fahrwerk und Räder
+	{"gear-rubber", 0, "SU39-PAINT7-DEF-01_SSF", false},
+	{"gear-metal", 0, "SU39-PAINT7-DEF-01_SSF", false},
+	{"gear-steel", 0, "SU39-PAINT7-DEF-01_SSF", false},
+
+	-- missing
+	{"gear-bay", 0, "SU39-PAINT8-DEF-01_SSF", false},
+
+	--{"pylon25lateral", 0, "Lateral25TPylon_gre", true},
+    {"pylon25lateral", 0, "Lateral25TPylon", true},
+
+	--{"RUS-PILOT", 0, "rus-pilot-2-gr", true},
+	--{"RUS-HELMET", 0, "rus-helmet-gr", true},
+}
+name = "105th Shadow Strike Force"
+--countries = {"GRC"}
