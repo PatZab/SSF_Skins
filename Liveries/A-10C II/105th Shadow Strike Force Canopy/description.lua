c8d9cd4fb0146170ff2ee79f08bcc122a06c88c8
funkpunkt <funkpunkt@gmx.de>
2025-11-27 22:09:24 +0100

    Add A-10C II SSF Canopy Livery

---
+++ Liveries/A-10C II/105th Shadow Strike Force Canopy/description.lua
@@ -0,0 +1,45 @@
+livery = {
+	{"A-10C_PAINT_1-L",	13			,	"a10cii_1l_roughmet", false};
+	{"A-10C_PAINT_1-g",	13			,	"a10cii_1g_roughmet", false};
+	{"A-10C_PAINT_1-e",	13			,	"a10cii_1e_roughmet", false};
+	{"A-10C_PAINT_1-j",	13			,	"a10cii_1j_roughmet", false};
+	{"A-10_Ruber Main",	13			,	"a10cii_1j_roughmet", false};
+	{"A-10_Ruber F",	13			,	"a10cii_1f_roughmet", false};
+	{"A-10C_PAINT_1-h",	13			,	"a10cii_1h_roughmet", false};
+	{"A-10C_PAINT_1-b",	13			,	"a10cii_1b_roughmet", false};
+	{"A-10C_PAINT_1-c",	13			,	"a10cii_1c_roughmet", false};
+	{"A-10C_PAINT_1-a",	13			,	"a10cii_1a_roughmet", false};
+	{"A-10C_PAINT_1-d",	13			,	"a10cii_1d_roughmet", false};
+	{"A-10C_PAINT_1-k",	13			,	"a10cii_1k_roughmet", false};
+	{"A-10C_PAINT_1-f",	13			,	"a10cii_1f_roughmet", false};
+	{"A-10C_PAINT_1-i",	13			,	"a10cii_1i_roughmet", false};
+	{"A-10C_PAINT_1-a", 0 ,"A-10C_a",false};
+	{"A-10C_PAINT_1-b", 0 ,"A-10C_b",false};
+	{"A-10C_PAINT_1-c", 0 ,"A-10C_c",false};
+	{"A-10C_PAINT_1-d", 0 ,"A-10C_d",false};
+	{"A-10C_PAINT_1-e", 0 ,"A-10C_e",false};
+	{"A-10C_PAINT_1-f", 0 ,"A-10C_f",false};
+	{"A-10C_PAINT_1-g", 0 ,"A-10C_g",false};
+	{"A-10C_PAINT_1-h", 0 ,"A-10C_h",false};
+	{"A-10C_PAINT_1-i", 0 ,"A-10C_i",false};
+	{"A-10C_PAINT_1-j", 0 ,"A-10C_j",false};
+	{"A-10C_PAINT_1-k", 0 ,"A-10C_k",false};
+	{"A-10C_PAINT_1-L", 0 ,"A-10C_l",false};
+	{"A-10_Number", 0 ,"empty",true};
+	{"A-10_Number_Noze_F", 0 ,"empty",true};
+	{"A-10_Number_Noze_T", 0 ,"empty",true};
+	{"A-10_Number_Wheel", 0 ,"empty",true};
+	{"pilot_A10_helmet",    DIFFUSE            ,    "pilot_a10_helmet", false};
+	{"pilot_A10_helmet_glass",	DIFFUSE			,	"pilot_a10_visor", false};
+	{"pilot_A10",    DIFFUSE            ,    "pilot_a10", false};
+	{"pilot_A10_helmet",    ROUGHNESS_METALLIC         ,    "pilot_a10_helmet_roughmet", true};
+}
+name = "105th Shadow Strike Force Canopy"
+countries = {"USA"}
+
+custom_args = 
+{
+ 
+[31] = -1.0,
+[32] = -1.0,
+}
